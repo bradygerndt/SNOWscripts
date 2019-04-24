61dba91e44142ccfb9ecadbf5d9741e46899d53d
Brady Gerndt <brady.gerndt@gmail.com>
2019-04-24 10:25:00 -0500

    Update aggregateFilter.js

--- utils/aggregateFilter.js
+++ utils/aggregateFilter.js
@@ -1,13 +1,8 @@
 /** 
-
  * Apply a number of aggregate functions as a condition to a standard filter (ie. Count, Average, Sum.)
-
  * Takes table name and encoded query string then returns an array of the specified field of records that meet the aggregate condition.
-
  * 
-
  * SCRIPTED FILTER EXAMPLE
-
  * [Serial Number] [is] [javascript:u_aggregate('cmdb_ci', 'install_status!=7', 'serial_number', 'COUNT', '>', 1)]
  * 
  * Read as: 
@@ -16,7 +11,6 @@
  * from the cmbd_ci table 
  * where install status does not equal 7 
  * and where serial number has a count of greater than 1.
-
  * @param {string} tableName - name of table for GlideRecord 
  * @param {string} encodedQuery - encoded query string 
  * @param {string} aggregator - aggregation function to be applied. (COUNT, SUM, MIN, MAX, AVG)
@@ -24,53 +18,48 @@
  * @param {int} value - value to be used in comparison to aggregator.
  * @param {string} groupBy - field to aggregate by.
  * @return {string[]} array of sys_id of sample records that meet criteria.
-
  */
 
 
 function u_aggregate(tableName, encodedQuery, groupBy, aggregator, operator, value) {
-	
-	try {
-		
-		var agg = new GlideAggregate(tableName);
-	
-		//Check input
-		if (!agg.isValid()) throw 'Invalid table name "' + tableName + '".';
-			
-		if (!agg.canRead()) throw 'No permission to read from "' + tableName + '".'; 
-		
-		if (agg.getElement(groupBy) == null) throw 'Cannot group by "' + groupBy + '"because it was not found in the provided table.';
-		
-		aggregator = aggregator.toUpperCase();
-		var aggCheck = ['COUNT','SUM','MIN','MAX','AVG'].indexOf(aggregator);
-		if (aggCheck < 0) throw 'Aggregate function must be AVG, COUNT or SUM.';
-		
-		var opCheck = ['>','<','>=','<=','=','!='].indexOf(operator);
-		if (opCheck < 0) throw "Operator must be one of (>, <, >=, <=, =, !=)";
-		//Check input
-		
-		var records = [];
-		
-		//query with aggregate
-		if (encodedQuery) agg.addQuery(encodedQuery);
-		agg.addAggregate(aggregator, groupBy);
-		agg.groupBy(groupBy);
-		agg.addHaving(aggregator, operator, value);
-		agg.query();
-		
-		
-		while(agg.next()){
-			records.push(agg.getValue(groupBy));
-		}
-		
-		return records;
-		
-	}
-	
-	catch (e) {
-		
-		return 'ERROR: ' + e;   // return error message
-		
-	}
-	
+
+    try {
+
+        var records = [];
+        var agg = new GlideAggregate(tableName);
+
+        //Check input
+        if (!agg.isValid()) throw 'Invalid table name "' + tableName + '".';
+
+        if (!agg.canRead()) throw 'No permission to read from "' + tableName + '".';
+
+        if (agg.getElement(groupBy) == null) throw 'Cannot group by "' + groupBy + '"because it was not found in the provided table.';
+
+        aggregator = aggregator.toUpperCase();
+        var aggCheck = ['COUNT', 'SUM', 'MIN', 'MAX', 'AVG'].indexOf(aggregator);
+        if (aggCheck < 0) throw 'Aggregate function must be AVG, COUNT or SUM.';
+
+        var opCheck = ['>', '<', '>=', '<=', '=', '!='].indexOf(operator);
+        if (opCheck < 0) throw "Operator must be one of (>, <, >=, <=, =, !=)";
+
+        //query with aggregate
+        if (encodedQuery) agg.addQuery(encodedQuery);
+        agg.addAggregate(aggregator, groupBy);
+        agg.groupBy(groupBy);
+        agg.addHaving(aggregator, operator, value);
+        agg.query();
+
+        while (agg.next()) {
+            records.push(agg.getValue(groupBy));
+        }
+
+        return records;
+
+    }
+    catch (e) {
+
+        return 'ERROR: ' + e;   // return error message
+
+    }
+
 }
